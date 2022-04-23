7e0b3e55e1f08c63aa01ef32c0218cc5e9c31db8
Indra Permana <48611701+indpermana@users.noreply.github.com>
2022-04-23 05:28:34 +0000

    Migrate menu

--- db/migrate/20220423040620_create_menus.rb
+++ db/migrate/20220423040620_create_menus.rb
@@ -2,7 +2,7 @@ class CreateMenus < ActiveRecord::Migration[7.0]
   def change
     create_table :menus do |t|
       t.string :name
-      
+      t.integer :price
       t.timestamps
     end
   end
